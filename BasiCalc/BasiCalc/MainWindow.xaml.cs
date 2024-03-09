a36bd79701668122b411af21154085d7f89d14eb
MonkySaint <151947041+MonkySaint@users.noreply.github.com>
2024-03-09 12:54:01 -0500

    lesson 001
    how to add
    button = BTN
    text box = TXT
    
    how to change and add text to TXT box

---
+++ BasiCalc/BasiCalc/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasiCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_ChangeTXT_Click(object sender, RoutedEventArgs e)
+        {
+            TXT_Box.Text = "it changed yay";
+
+        }
+
+        private void Btn_AddTXT_Click(object sender, RoutedEventArgs e)
+        {
+            TXT_Box.Text += " <-- the fist part is a lie";
+        }
+
+        private void TXT_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
